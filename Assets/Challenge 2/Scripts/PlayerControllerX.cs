3eea117cf68c80b4d204e0a5ac95537e3a1df1ca
Emegbue Chukwudi John <ecj4real@gmail.com>
2022-09-10 19:24:43 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private static DateTime defaultTime;
+    private int allowSpawnTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DateTime currentTime = DateTime.Now;
+            if ((currentTime - defaultTime).TotalSeconds >= allowSpawnTime)
+            {
+                defaultTime = DateTime.Now;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            }
+        }
+    }
+}
